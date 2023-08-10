8d4fe914d1da78f1750163d2f3b04365899ab788
Riyad <Riyad9876@gmail.com>
2023-08-10 13:06:24 -0400

    stateful form data AddPostForm

--- src/components/AddPostForm.js
+++ src/components/AddPostForm.js
@@ -1,17 +1,32 @@
-
+import { useState } from "react"
 
 export default function AddPostForm(){
+    const [formData, setFormData] = useState({
+        title: '',
+        content: ''
+    })
+
+    const handleSubmit = (e) =>{
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return(
         <div>
-
-            <form>
+            {/* onSubmit */}
+            <form >
                 <label htmlFor="title">Title: </label>
-                <input id='title' type="text"/>
+                <input value={formData.title} onChange={handleSubmit} name='title' id='title' type="text"/>
+                {/* onChange={(e) => setFormData({title : e.value})} */}
+
                 <br/>
                 <br/>
                 
                 <label htmlFor="content">Content: </label>
-                <textarea id="content"/>
+                <textarea value={formData.content} onChange={handleSubmit} name="content" id="content"/>
+                {/* onChange={(e) => setFormData({content: e.value})} */}
 
                 <button> Add Post</button>
             </form>
