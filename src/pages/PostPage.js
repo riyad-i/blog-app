8c2637f3d455616fa799baa45d8c43b55dcc3792
Riyad <Riyad9876@gmail.com>
2023-08-10 15:14:05 -0400

    get post by id

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,10 +1,13 @@
 import PostItem from "../components/PostItem"
 import {useParams} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 export default function PostPage() {
     const {id} = useParams()
 
-    console.log(id);
+    const post = useSelector(state => state.posts.find(post => post.id === id))
+
+    console.log(post);
 
     return (
         <h1>Post Page</h1>
