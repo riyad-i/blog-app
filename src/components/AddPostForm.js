f40a64a1260b433e437849c6c1cb38ce1eab2cfe
Riyad <Riyad9876@gmail.com>
2023-08-10 14:23:07 -0400

    handleSubmit, reset form data, dispatch, addPost reducer

--- src/components/AddPostForm.js
+++ src/components/AddPostForm.js
@@ -1,4 +1,9 @@
 import { useState } from "react"
+import { addPost } from "../redux/slices/postsSlice"
+import { useDispatch } from "react-redux"
+import { nanoid } from "@reduxjs/toolkit"
+
+
 
 export default function AddPostForm(){
     const [formData, setFormData] = useState({
@@ -6,26 +11,45 @@ export default function AddPostForm(){
         content: ''
     })
 
-    const handleSubmit = (e) =>{
+    const dispatch = useDispatch()
+
+    const handleChange = (e) =>{
         setFormData({
             ...formData,
             [e.target.name]: e.target.value
         })
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const newPost = {...formData, id: nanoid()}
+
+        //dispatch action
+        dispatch(addPost(newPost))
+
+        //reset form state
+
+        setFormData({
+            title: '',
+            content: ''
+        })
+
+    }
+
     return(
         <div>
             {/* onSubmit */}
-            <form >
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="title">Title: </label>
-                <input value={formData.title} onChange={handleSubmit} name='title' id='title' type="text"/>
+                <input value={formData.title} onSubmit={handleSubmit} onChange={handleChange} name='title' id='title' type="text"/>
                 {/* onChange={(e) => setFormData({title : e.value})} */}
 
                 <br/>
                 <br/>
                 
                 <label htmlFor="content">Content: </label>
-                <textarea value={formData.content} onChange={handleSubmit} name="content" id="content"/>
+                <textarea value={formData.content} onSubmit={handleSubmit} onChange={handleChange} name="content" id="content"/>
                 {/* onChange={(e) => setFormData({content: e.value})} */}
 
                 <button> Add Post</button>
--- src/redux/slices/postsSlice.js
+++ src/redux/slices/postsSlice.js
@@ -1,18 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
-export const postsSlice= createSlice({
+export const postsSlice = createSlice({
     name: 'posts',
     initialState: [
         {id: '1', title: 'first post', content: 'Hello!'},
         {id: '2', title: 'second post', content: 'More Text!'}
     ],
     reducers:{
-
+        addPost: (state, action) => {
+            const {payload} = action
+            console.log(payload);
+        }
     }
 })
 
 
 //export action
 
+export const {addPost} = postsSlice.actions
 export default postsSlice.reducer;
